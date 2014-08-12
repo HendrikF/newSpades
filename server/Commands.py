b052f71801685193e68c60812ab339ad9a483bea
KingEik <KingEik@users.noreply.github.com>
2014-08-12 16:28:11 +0200

    Added basic protocol (server)

--- server/Commands.py
+++ server/Commands.py
@@ -1,50 +1,59 @@
 
 
 def getCommands(parent):
-	def stop(self, args):
-		self.stopNow = True
-		print("Stopping server...")
-	parent.addCommand(stop, "stop", "Shuts the server down. No arguments.")
-	
-	def help(self, args):
-		if len(args) == 0:
-			str = "Aviable commands: "
-			for x in self.commands.keys():
-				str += x+" "
-			print(str)
-		else:
-			if args[0] in self.help.keys():
-				if self.help[args[0]] is None:
-					print("No help aviable!")
-				else:
-					print(self.help[args[0]])
-			else:
-				print("Command does not exist!")
-	parent.addCommand(help, "help", "Shows all commands or help for single command.\nUsage: help [commandname]")
-	
-	def master(self, args):
-		self.config["master"] = not self.config["master"]
-		print("Master broadcast toggled "+ ("ON" if self.config["master"] else "OFF")+"!")
-	parent.addCommand(master, "master", "Switches master broadcast ON/OFF. No arguments.")
-	
-	def show_players(self, args):
-		print("Currently", self.numberOfPlayers, "players online.")
-	parent.addCommand(show_players, "list", "Shows number of players online. No arguments.")
+    def stop(self, args):
+        self.stopNow = True
+        print("Stopping server...")
+    parent.addCommand(stop, "stop", "Shuts the server down. No arguments.")
+    
+    def help(self, args):
+        if len(args) == 0:
+            str = "Aviable commands: "
+            for x in self.commands.keys():
+                str += x+" "
+            print(str)
+        else:
+            if args[0] in self.help.keys():
+                if self.help[args[0]] is None:
+                    print("No help aviable!")
+                else:
+                    print(self.help[args[0]])
+            else:
+                print("Command does not exist!")
+    parent.addCommand(help, "help", "Shows all commands or help for single command.\nUsage: help [commandname]")
+    
+    def master(self, args):
+        self.config["master"] = not self.config["master"]
+        print("Master broadcast toggled "+ ("ON" if self.config["master"] else "OFF")+"!")
+    parent.addCommand(master, "master", "Switches master broadcast ON/OFF. No arguments.")
+    
+    def show_players(self, args):
+        print("Currently", self.numberOfPlayers, "players online.")
+    parent.addCommand(show_players, "list", "Shows number of players online. No arguments.")
+    
+    def getPlayerNames(server):
+        names = []
+        for x in server.players:
+            names.append(x.name)
+        return names
+    parent.getPlayerNames = getPlayerNames
 
 def getHooks(Hooks):
-	class ScriptHooks(Hooks):
-		def hook_Start(self):
-			#print("Hello!")
-			Hooks.hook_Start(self)
-		
-		def hook_Run(self):
-			Hooks.hook_Run(self)
-		
-		def hook_Stop(self):
-			#print("Goodbye!")
-			Hooks.hook_Stop(self)
-	return ScriptHooks
-	
-	
-	
-	
+    class ScriptHooks(Hooks):
+        def hook_Start(self):
+            #print("Hello!")
+            Hooks.hook_Start(self)
+        
+        def hook_Run(self):
+            Hooks.hook_Run(self)
+        
+        def hook_Stop(self):
+            #print("Goodbye!")
+            Hooks.hook_Stop(self)
+    return ScriptHooks
+
+
+
+
+
+

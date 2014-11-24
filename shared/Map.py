975976563e2bff498ee574e900e78c6f76baa2eb
Hendrik Fritsch <hendrik.fritsch@web.de>
2014-11-24 23:32:45 +0100

    Added a little but beautiful and impressive shadow to edges and so on to better differentiate the blocks

--- shared/Map.py
+++ shared/Map.py
@@ -11,19 +11,6 @@ logger = logging.getLogger(__name__)
 
 SECTOR_SIZE = 16
 
-def cubeVertices(x, y, z, d=0.5):
-    """Returns a list of all verticies of the block at (x, y, z)"""
-    e = d-0.5
-    f = d+0.5
-    return [
-        x-d, y+e, z-d, x-d, y+e, z+d, x+d, y+e, z+d, x+d, y+e, z-d, # top
-        x-d, y-f, z-d, x+d, y-f, z-d, x+d, y-f, z+d, x-d, y-f, z+d, # bottom
-        x-d, y-f, z-d, x-d, y-f, z+d, x-d, y+e, z+d, x-d, y+e, z-d, # left
-        x+d, y-f, z+d, x+d, y-f, z-d, x+d, y+e, z-d, x+d, y+e, z+d, # right
-        x-d, y-f, z+d, x+d, y-f, z+d, x+d, y+e, z+d, x-d, y+e, z+d, # front
-        x+d, y-f, z-d, x-d, y-f, z-d, x-d, y+e, z-d, x+d, y+e, z-d, # back
-    ]
-
 def sectorize(position):
     """Returns the sector in which a block is located"""
     x, y, z = position
@@ -66,8 +53,13 @@ class Map(object):
     
     def _load(self):
         for x in range(0, 50):
+            self.addBlock((x, 1, 0), (1, 1, 0), immediate=False)
             for z in range(0, 50):
                     self.addBlock((x, 0, z), (0, 1, 0), immediate=False)
+        self.addBlock((4, 0, 0), (1, 0, 0), immediate=False)
+        self.addBlock((0, 4, 0), (0.5, 0, 0), immediate=False)
+        self.addBlock((0, 0, 4), (0, 0, 0), immediate=False)
+        self.addBlock((4, 4, 4), (1, 1, 1), immediate=False)
     
     def calculateDimensions(self):
         dx = 0
@@ -135,7 +127,7 @@ class Map(object):
         block = self.getBlocksLookingAt(position, vector, maxDistance)[0]
         if block:
             x, y, z = block
-            vertex_data = cubeVertices(x, y, z, 0.51)
+            vertex_data = self.cubeVertices(x, y, z, 0.51)
             glColor3d(0, 0, 0)
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
             glLineWidth(width)
@@ -166,22 +158,30 @@ class Map(object):
             else:
                 if key in self.shown:
                     self.hideBlock(key)
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                for dz in [-1, 0, 1]:
+                    #if dx==dy==0 or dx==dz==0 or dy==dz==0:
+                    if dx==dy==dz==0:
+                        continue
+                    pos = (x + dx, y + dy, z + dz)
+                    if pos in self.world:
+                        self.updateBlock(pos)
     
     def showBlock(self, position, immediate=True):
-        color = self.world[position]
-        self.shown[position] = color
+        self.shown[position] = self.world[position] # color
         if immediate:
-            self._showBlock(position, color)
+            self._showBlock(position)
         else:
-            self._enqueue(self._showBlock, position, color)
+            self._enqueue(self._showBlock, position)
     
-    def _showBlock(self, position, color):
+    def _showBlock(self, position):
         x, y, z = position
-        vertex_data = cubeVertices(x, y, z)
-        color_data = list(color)
+        vertex_data = self.cubeVertices(x, y, z)
+        color_data = self.vertexColors(x, y, z, vertex_data)
         self._shown[position] = self.batch.add(24, GL_QUADS, None,
             ('v3f/static', vertex_data),
-            ('c3f/static', color_data*24)
+            ('c3f/static', color_data)
         )
     
     def hideBlock(self, position, immediate=True):
@@ -193,6 +193,10 @@ class Map(object):
     
     def _hideBlock(self, position):
         self._shown.pop(position).delete()
+    
+    def updateBlock(self, position):
+        self.hideBlock(position)
+        self.showBlock(position)
     
     def showSector(self, sector):
         for position in self.sectors.get(sector, []):
@@ -241,3 +245,98 @@ class Map(object):
     def process_entire_queue(self):
         while self.queue:
             self._dequeue()
+    
+    def cubeVertices(self, x, y, z, d=0.5):
+        """Returns a list of all vertices of the block at (x, y, z)"""
+        e = d-0.5 # 0
+        f = d+0.5 # 1
+        return [
+            x-d, y+e, z-d,   x-d, y+e, z+d,   x+d, y+e, z+d,   x+d, y+e, z-d, # top
+            x-d, y-f, z-d,   x+d, y-f, z-d,   x+d, y-f, z+d,   x-d, y-f, z+d, # bottom
+            x-d, y-f, z-d,   x-d, y-f, z+d,   x-d, y+e, z+d,   x-d, y+e, z-d, # front
+            x+d, y-f, z+d,   x+d, y-f, z-d,   x+d, y+e, z-d,   x+d, y+e, z+d, # back
+            x-d, y-f, z+d,   x+d, y-f, z+d,   x+d, y+e, z+d,   x-d, y+e, z+d, # right
+            x+d, y-f, z-d,   x-d, y-f, z-d,   x-d, y+e, z-d,   x+d, y+e, z-d, # left
+        ]
+    
+    def getLightLevel(self, position, f):
+        """Parameters are the coordinates of an edge and 0-5 for top, bottom, ..., left (see cubeVertices())
+        Returns the lightlevel of a vertex depending on for which face it is drawn
+        0    = 4 blocks covering vertex
+        0.25 = 3
+        0.5  = 2
+        0.75 = 1
+        1    = 0
+        """
+        x, y, z = position
+        m = [
+            [ # top
+                (x-0.5, y+1, z+0.5), 
+                (x-0.5, y+1, z-0.5), 
+                (x+0.5, y+1, z-0.5), 
+                (x+0.5, y+1, z+0.5)
+            ],
+            [ # bottom
+                (x-0.5, y  , z+0.5), 
+                (x-0.5, y  , z-0.5), 
+                (x+0.5, y  , z-0.5), 
+                (x+0.5, y  , z+0.5)
+            ],
+            [ # front
+                (x-0.5, y  , z+0.5), 
+                (x-0.5, y  , z-0.5), 
+                (x-0.5, y+1, z-0.5), 
+                (x-0.5, y+1, z+0.5)
+            ],
+            [ # back
+                (x+0.5, y  , z+0.5), 
+                (x+0.5, y  , z-0.5), 
+                (x+0.5, y+1, z-0.5), 
+                (x+0.5, y+1, z+0.5)
+            ],
+            [ # right
+                (x+0.5, y  , z+0.5), 
+                (x-0.5, y  , z+0.5), 
+                (x-0.5, y+1, z+0.5), 
+                (x+0.5, y+1, z+0.5)
+            ],
+            [ # left
+                (x+0.5, y  , z-0.5), 
+                (x-0.5, y  , z-0.5), 
+                (x-0.5, y+1, z-0.5), 
+                (x+0.5, y+1, z-0.5)
+            ]
+        ]
+        return sum([0 if pos in self.world else 0.25 for pos in m[f]])
+    
+    def vertexColors(self, x, y, z, vertex_data):
+        """For each vertex look which lightlevel it has and modify its color"""
+        r, g, b = self.world[(x, y, z)]
+        color_data = []
+        for i in range(24): # len(vertex_data) / 3
+            ll = self.getLightLevel(vertex_data[i*3:i*3+3], i//4)
+            color_data.extend((r*ll, g*ll, b*ll))
+        return color_data
+
+"""
+    (x+d|y+e|z-d) o------------------o (x+d|y+e|z+d)                                
+                 /.    (x|y|z)      /|                                              
+                / .       X        / |                                              
+               /  .               /  |                               Y              
+(x-d|y+e|z-d) o------------------o (x-d|y+e|z+d)                   ^                
+              |   .              |   |                             |    X           
+              |   .              |   |                             |  ^             
+              |   .              |   |                             | /              
+              |   .              |   |                             |/               
+              |   .              |   |                           --O--------> Z     
+    (x+d|y-f|z-d) ...............|...o (x+d|y-f|z+d)              /|                
+              |  .               |  /                                               
+              | .                | /                                                
+              |.                 |/                                                 
+(x-d|y-f|z-d) o------------------o (x-d|y-f|z+d)                                    
+    
+    * cubeVertices() returns the faces ANTICLOCKWISE                                
+    * the coordinate of the block lies on top of it in the middle                   
+
+"""
+

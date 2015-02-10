2b7a8fa672aacf7c5baf6fc8678f157a9ba3d4da
Hendrik Fritsch <hendrik.fritsch@web.de>
2015-02-10 20:43:09 +0100

    Added command line options to newspades.py for autoconnect

--- client/Launcher.py
+++ client/Launcher.py
@@ -8,11 +8,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Launcher(Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, **kw):
         Frame.__init__(self, master)
         # http://stackoverflow.com/a/4140988/3447531
         # %P = value of the entry if the edit is allowed
         self._validatePort = (self.master.register(self.validatePort), '%P')
+        # For Auto-connect
+        self.presets = kw
         self.build()
     
     def build(self):
@@ -26,15 +28,15 @@ class Launcher(Frame):
         Label(self, text='Username').grid(column=0, row=2)
         
         self.addr = Entry(self, justify=CENTER)
-        self.addr.insert(0, 'localhost')
+        self.addr.insert(0, self.presets.get('host', 'localhost'))
         self.addr.grid(column=0, row=1)
         
         self.port = Entry(self, justify=CENTER, validate=ALL, validatecommand=self._validatePort)
-        self.port.insert(0, '55555')
+        self.port.insert(0, self.presets.get('port', '55555'))
         self.port.grid(column=1, row=1)
         
         self.username = Entry(self, justify=CENTER)
-        self.username.insert(0, 'Greenhorn')
+        self.username.insert(0, self.presets.get('username', 'Greenhorn'))
         self.username.grid(column=1, row=2)
         
         self.progressbar = Progressbar(self, maximum=6)
--- newspades.py
+++ newspades.py
@@ -4,6 +4,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-sw', dest='no_shadow_window', action='store_true', help="Don't create pyglets shadow window")
 parser.add_argument('-l', '--loglevel', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Set the severity threshold of logged messages')
+parser.add_argument('host', nargs='?', help='Host to connect to (no port)')
+parser.add_argument('-p', '--port', type=int, help='Port at host to connect to')
+parser.add_argument('-u', '--username', help='Username to connect with')
+parser.add_argument('-a', '--auto', action='store_true', help='Automatically connect to server')
 args = parser.parse_args()
 
 import shared.logging
@@ -19,5 +23,17 @@ if args.no_shadow_window:
 
 from client.Launcher import Launcher
 
-launcher = Launcher()
+kw = {}
+if args.host:
+    kw.update(host=args.host)
+if args.port:
+    kw.update(port=args.port)
+if args.username:
+    kw.update(username=args.username)
+
+launcher = Launcher(**kw)
+
+if args.auto:
+    launcher.connect()
+
 launcher.mainloop()

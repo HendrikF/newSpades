52a17398bce7aae85ac53ea9c1250256a0fb7d56
Hendrik Fritsch <hendrik.fritsch@web.de>
2015-04-08 20:01:41 +0200

    Removed transmitter from repo

--- transmitter/error.py
+++
@@ -1,6 +0,0 @@
-class TransmitterError(Exception): pass
-class InvalidFieldFormat(   ValueError, TransmitterError): pass
-class MessageNotFound(      KeyError,   TransmitterError): pass
-class InvalidMessageField(  KeyError,   TransmitterError): pass
-class DuplicateMessageID(   ValueError, TransmitterError): pass
-class PeerNotFound(         KeyError,   TransmitterError): pass
